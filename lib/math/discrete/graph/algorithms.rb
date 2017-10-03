3a032f9c0288f5e671f65847c7710ac211dce0b2
mac-adam-chaieb <moe.chaieb@shopify.com>
2017-10-03 16:02:01 -0400

    Add documentation and resources to algorithms and properties

--- lib/math/discrete/graph/algorithms.rb
+++ lib/math/discrete/graph/algorithms.rb
@@ -4,6 +4,10 @@ require 'union_find'
 module Math::Discrete::Graph::Algorithms
   class NegativeWeightCycleError < StandardError; end
 
+  # Breadth-first search (BFS) is an algorithm for traversing a graph
+  # It starts at the givenroot and explores the neighbor nodes first, before moving to the next level neighbours.
+  # Read more:
+  # https://en.wikipedia.org/wiki/Breadth-first_search
   def breadth_first_search(root: vertex_set.first)
     return {} if @vertex_map.empty?
 
@@ -32,6 +36,10 @@ module Math::Discrete::Graph::Algorithms
     search_tree
   end
 
+  # Depth-first search (DFS) is an algorithm for traversing a graph
+  # It starts at the given root and explores as far as possible along each branch before backtracking.
+  # Read more:
+  # https://en.wikipedia.org/wiki/Depth-first_search
   def depth_first_search(root: vertex_set.first)
     return {} if @vertex_map.empty?
 
@@ -60,6 +68,10 @@ module Math::Discrete::Graph::Algorithms
     search_tree
   end
 
+  # The shortest path finds a path between two vertices (or nodes) in the graph such that the sum of the weights of
+  # its constituent edges is minimized.
+  # Read more:
+  # https://en.wikipedia.org/wiki/Shortest_path_problem
   def shortest_path_between(source, target)
     raise TypeError, 'source and target vertices must be of type Vertex' unless source.is_a?(Vertex) && target.is_a?(Vertex)
     raise Graph::VertexNotFound, "could not find vertex with label=#{source.label}" unless vertex_labels.include? source.label
@@ -83,6 +95,11 @@ module Math::Discrete::Graph::Algorithms
     Graph::Path[*edges.reverse]
   end
 
+  # A minimum spanning tree (MST) is a subset of the edges of a connected, edge-weighted undirected graph that
+  # connects all the vertices together, without any cycles and with the minimum possible total edge weight.
+  # That is, it is a spanning tree whose sum of edge weights is as small as possible.
+  # Read more:
+  # https://en.wikipedia.org/wiki/Minimum_spanning_tree
   def minimum_spanning_tree
     minimum_edge_set = Set[]
     union_set = UnionFind::UnionFind.new vertex_set
@@ -100,6 +117,10 @@ module Math::Discrete::Graph::Algorithms
 
   private
 
+  # Dijkstra's algorithm is an algorithm for finding the shortest paths between nodes in a graph.
+  # The graph must not have negative-weighted edges
+  # Read more:
+  # https://en.wikipedia.org/wiki/Dijkstra's_algorithm
   def dijkstras_algorithm(source, target)
     distance_tree = { source.label => { distance: 0, parent: nil } }
     priority_queue = Containers::PriorityQueue.new do |a,b|
@@ -129,6 +150,11 @@ module Math::Discrete::Graph::Algorithms
     distance_tree
   end
 
+  # The Bellman–Ford algorithm is an algorithm that computes shortest paths from a source vertex to all of the other
+  # vertices in a weighted graph. It is slower than Dijkstra's algorithm for the same problem, but more versatile,
+  # as it is capable of handling graphs in which some of the edge weights are negative numbers.
+  # Read more:
+  # https://en.wikipedia.org/wiki/Bellman–Ford_algorithm
   def bellman_fords_algorithm(source, target)
     distance_tree = { source.label => { distance: 0, parent: nil } }
 
